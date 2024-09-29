1358e7116c902d64bb33ccd4e086fa95e06dab11
avallenn <avallen@syr.edu>
2024-09-29 17:26:34 -0400

    pandaslib

--- code/pandaslib.py
+++ code/pandaslib.py
@@ -9,7 +9,7 @@ def get_column_names(df : pd.DataFrame) -> list[str]:
     Get all column names of a pandas dataframe df
     Returns the names as a list of string
     '''
-    pass # todo: replace this line and add your code
+    return df.columns.tolist()
 
 
 def get_columns_of_type(df : pd.DataFrame, numpy_type: any) -> list[str]:
@@ -17,14 +17,21 @@ def get_columns_of_type(df : pd.DataFrame, numpy_type: any) -> list[str]:
     Return the column names of a pandas dataframe only when 
     the values in the column match the numpy_type
     '''
-    pass # todo: replace this line and add your code
+    columns = []
+    for c in df.columns:
+        if df[c].dtype == numpy_type:
+            columns.append(c)
+    return columns
+
 
 
 def get_unique_values(df : pd.DataFrame, column_name: str) -> list:
     '''
     Get a list of unique values of a column in a pandas dataframe
     '''
-    pass # todo: replace this line and add your code
+    uniquecol = df[column_name].unique()
+    uniquelist = uniquecol.tolist()
+    return uniquelist
 
 def get_file_extension(file_path : str) -> str:
     '''
@@ -34,7 +41,9 @@ def get_file_extension(file_path : str) -> str:
     'countries.json' -> 'json'
 
     '''
-    pass # todo: replace this line and add your code
+    filesplit = file_path.split('.')
+    return filesplit[-1]
+
 
 def load_file(file_path: str, ext: str) -> pd.DataFrame:
     '''
@@ -44,7 +53,17 @@ def load_file(file_path: str, ext: str) -> pd.DataFrame:
     - when csv assume first row is header
     - when json assume record-oriented data
     '''
-    pass # todo: replace this line and add your code
+    if ext == 'csv':
+        csv = pd.read_csv(file_path, header=0)
+        return csv
+    if ext == 'json':
+        json = pd.read_json(file_path, orient='records')
+        return json
+    if ext == 'xlsx':
+        xlsx = pd.read_excel(file_path)
+        return xlsx
+    else: 
+        return None
 
 if __name__ == '__main__':
     df = pd.DataFrame({ 
--- code/unibrow.py
+++ code/unibrow.py
@@ -4,9 +4,26 @@ Solution unibrow.py
 import pandas as pd
 import streamlit as st
 import pandaslib as pl
+from pandaslib import get_column_names, get_columns_of_type, get_unique_values, get_file_extension, load_file
 
 st.title("UniBrow")
 st.caption("The Universal data browser")
 
-# TODO Write code here to complete the unibrow.py
+file = st.file_uploader("Upload a file", type=['csv', 'xlsx', 'json'])
+if file:
+    file_path = get_file_extension(file.name)
+    df = load_file(file, file_path)
+    columns = get_column_names(df)
+    selected_columns = st.multiselect("Select columns to display", columns)
+    if st.checkbox("Filter data"):
+        type_columns = get_columns_of_type(df, 'object')
+        filter_column = st.selectbox("Select column to filter", type_columns)
+        if filter_column:
+            values = get_unique_values(df, filter_column)
+            value = st.selectbox("Select value to filter on", values)
+            df_show = df[df[filter_column] == value][selected_columns]
+    else:
+        df_show = df[selected_columns]
+
+    st.dataframe(df_show)
 
